0e13641018ba6a09ab8650af7efeee5f713600ea
Natavan Hasanova <natavanhesenova4@gmail.com>
2025-02-19 23:56:01 +0400

    Update the App.js to conditionally hide the footer on the signup page.

--- verifai-frontend/src/App.js
+++ verifai-frontend/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
+  const location = useLocation(); 
+
   return (
     <>
       <Navbar />
       <div style={{ minHeight: "85vh" }}> {/* Ensures Footer stays at bottom */}
         <AppRoutes />
       </div>
-      <Footer />
+      {/* Hide Footer only on the Signup Page */}
+      {location.pathname !== "/signup" && <Footer />}
     </>
   );
 };
