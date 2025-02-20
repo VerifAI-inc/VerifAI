b90f6e787ab40a5af915768e28b0c3675c7dd6f7
khaleed_mammad <khaleed.mammad@gmail.com>
2025-02-20 19:54:14 +0400

    added ml module

---
+++ verifai-ml/__init__.py
@@ -0,0 +1,8 @@
+"""
+├── ml_module/
+│   ├── __init__.py
+│   ├── data_processing.py      # For dataset loading and validation
+│   ├── model_loader.py         # For loading and validating the model
+│   ├── evaluation.py           # For computing accuracies, MIA, and fairness metrics
+│   └── reporting.py            # For formatting and saving results
+"""
