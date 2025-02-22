e736c666f25bdc83fe9a340c6b1ffcd78fa12d2b
Natavan Hasanova <natavanhesenova4@gmail.com>
2025-02-22 15:48:02 +0400

    Modify the Signup Page to add a central div for aligning footer.

--- verifai-frontend/src/pages/Signup.js
+++ verifai-frontend/src/pages/Signup.js
@@ -26,64 +26,66 @@ const Signup = () => {
   };
 
   return (
-    <div className="signup-container">
-      {/* Left Side (Branding) */}
-      <div className="signup-left">
-        <h2>Welcome to VerifAI!</h2>
-        <p>Ensure fairness, protect privacy, build trust.</p>
-      </div>
+    <div className="signup-page">
+      <div className="signup-container">
+        {/* Left Side (Branding) */}
+        <div className="signup-left">
+          <h2>Welcome to VerifAI!</h2>
+          <p>Ensure fairness, protect privacy, build trust.</p>
+        </div>
 
-      {/* Right Side: Signup Form */}
-      <div className="signup-right">
-        <h1 className="signup-title">Create an Account</h1>
-        <form className="signup-form" onSubmit={handleSubmit}>
+        {/* Right Side: Signup Form */}
+        <div className="signup-right">
+          <h1 className="signup-title">Create an Account</h1>
+          <form className="signup-form" onSubmit={handleSubmit}>
 
-          {/* First Row - First Name & Last Name */}
-          <div className="row">
-            <div className="form-group">
-              <label>First Name <span className="required">*</span></label>
-              <input type="text" name="firstName" placeholder="Enter first name" required onChange={handleChange} />
+            {/* First Row - First Name & Last Name */}
+            <div className="row">
+              <div className="form-group">
+                <label>First Name <span className="required">*</span></label>
+                <input type="text" name="firstName" placeholder="Enter first name" required onChange={handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Last Name <span className="required">*</span></label>
+                <input type="text" name="lastName" placeholder="Enter last name" required onChange={handleChange} />
+              </div>
             </div>
-            <div className="form-group">
-              <label>Last Name <span className="required">*</span></label>
-              <input type="text" name="lastName" placeholder="Enter last name" required onChange={handleChange} />
-            </div>
-          </div>
 
-          {/* Second Row - Username & Email */}
-          <div className="row">
-            <div className="form-group">
-              <label>Username <span className="required">*</span></label>
-              <input type="text" name="username" placeholder="Choose a username" required onChange={handleChange} />
+            {/* Second Row - Username & Email */}
+            <div className="row">
+              <div className="form-group">
+                <label>Username <span className="required">*</span></label>
+                <input type="text" name="username" placeholder="Choose a username" required onChange={handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Email <span className="required">*</span></label>
+                <input type="email" name="email" placeholder="Enter your email" required onChange={handleChange} />
+              </div>
             </div>
-            <div className="form-group">
-              <label>Email <span className="required">*</span></label>
-              <input type="email" name="email" placeholder="Enter your email" required onChange={handleChange} />
-            </div>
-          </div>
 
-          {/* Third Row - Password & Confirm Password */}
-          <div className="row">
-            <div className="form-group">
-              <label>Password <span className="required">*</span></label>
-              <input type="password" name="password" placeholder="Enter your password" required onChange={handleChange} />
+            {/* Third Row - Password & Confirm Password */}
+            <div className="row">
+              <div className="form-group">
+                <label>Password <span className="required">*</span></label>
+                <input type="password" name="password" placeholder="Enter your password" required onChange={handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Confirm Password <span className="required">*</span></label>
+                <input type="password" name="confirmPassword" placeholder="Re-enter your password" required onChange={handleChange} />
+              </div>
             </div>
-            <div className="form-group">
-              <label>Confirm Password <span className="required">*</span></label>
-              <input type="password" name="confirmPassword" placeholder="Re-enter your password" required onChange={handleChange} />
+
+            {/* Sign Up Button */}
+            <button type="submit" className="signup-button">Sign Up</button>
+
+            {/* Login Redirect */}
+            <div className="signup-footer">
+              <p>Already have an account? 
+                <Link to="/login" className="login-link"> Log in here</Link>.
+              </p>
             </div>
-          </div>
-
-          {/* Sign Up Button */}
-          <button type="submit" className="signup-button">Sign Up</button>
-
-          {/* Login Redirect */}
-          <div className="signup-footer">
-            <p>Already have an account? 
-              <Link to="/login" className="login-link"> Log in here</Link>.
-            </p>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
