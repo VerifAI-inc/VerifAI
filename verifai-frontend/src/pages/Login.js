f2276bbb4a922440fafa52c0d3cc9eec8f793c68
Behruz Gurbanli <behruzgurbanli@gmail.com>
2025-04-27 22:13:32 +0400

    api url corrected

--- verifai-frontend/src/pages/Login.js
+++ verifai-frontend/src/pages/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
     };
   
     try {
-      const response = await fetch("http://127.0.0.1:8000/auth/login/", {
+      const response = await fetch("http://127.0.0.1:8000/api/auth/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
