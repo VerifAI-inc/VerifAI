255bddb62da3def62065d6fe3e54b4358ddd3d31
Behruz Gurbanli <behruzgurbanli@gmail.com>
2025-04-28 18:05:35 +0400

    logo add

--- verifai-frontend/src/components/Navbar.js
+++ verifai-frontend/src/components/Navbar.js
@@ -10,28 +10,62 @@ const Navbar = () => {
     <nav className="navbar">
       {/* Logo Section */}
       <div className="navbar-logo">
-        <span>VerifAI</span>
+        <Link to="/" onClick={() => setMenuOpen(false)}>
+          <img src="/logo.png" alt="VerifAI Logo" className="navbar-logo-img" />
+        </Link>
       </div>
 
       {/* Menu Links */}
       <ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
-        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-        <li><Link to="/upload" onClick={() => setMenuOpen(false)}>Upload</Link></li>
+        <li>
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/upload" onClick={() => setMenuOpen(false)}>
+            Upload
+          </Link>
+        </li>
 
         {isLoggedIn && (
           <>
-            <li><Link to="/results" onClick={() => setMenuOpen(false)}>Results</Link></li>
-            <li><Link to="/reports" onClick={() => setMenuOpen(false)}>Reports</Link></li>
-            <li><Link to="/tables" onClick={() => setMenuOpen(false)}>Tables</Link></li>
-            <li><Link to="/profile" onClick={() => setMenuOpen(false)}>Profile</Link></li>
+            <li>
+              <Link to="/results" onClick={() => setMenuOpen(false)}>
+                Results
+              </Link>
+            </li>
+            <li>
+              <Link to="/reports" onClick={() => setMenuOpen(false)}>
+                Reports
+              </Link>
+            </li>
+            <li>
+              <Link to="/tables" onClick={() => setMenuOpen(false)}>
+                Tables
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" onClick={() => setMenuOpen(false)}>
+                Profile
+              </Link>
+            </li>
           </>
         )}
 
         {!isLoggedIn && (
-          <li><Link to="/login" onClick={() => setMenuOpen(false)}>Login</Link></li>
+          <li>
+            <Link to="/login" onClick={() => setMenuOpen(false)}>
+              Login
+            </Link>
+          </li>
         )}
 
-        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+        <li>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Mobile Menu Toggle */}
