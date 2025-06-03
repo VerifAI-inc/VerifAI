89285eafa5adb2239a41fd4d7a15ce9fffb219fa
Behruz Gurbanli <behruzgurbanli@gmail.com>
2025-06-04 00:34:44 +0400

    tailing slashes removed

--- verifai-frontend/src/config.js
+++ verifai-frontend/src/config.js
@@ -1,9 +1,9 @@
 let API_BASE_URL = "";
 
 if (window.location.hostname === "localhost") {
-  API_BASE_URL = "http://localhost:8000/";
+  API_BASE_URL = "http://localhost:8000";
 } else {
-  API_BASE_URL = "https://verifai-backend.onrender.com/";
+  API_BASE_URL = "https://verifai-backend.onrender.com";
 }
 
 export default API_BASE_URL;
