10eb165059125b59991727fdbbf5317506243e2d
Behruz Gurbanli <behruzgurbanli@gmail.com>
2025-06-04 00:13:27 +0400

    Update config.js

--- verifai-frontend/src/config.js
+++ verifai-frontend/src/config.js
@@ -1,6 +1,17 @@
-const backendHost = window.location.hostname;
-const backendPort = "8000";
+let API_BASE_URL = "";
 
-const API_BASE_URL = `http://${backendHost}:${backendPort}/`;
+if (window.location.hostname === "localhost") {
+  API_BASE_URL = "http://localhost:8000/";
+} else {
+  API_BASE_URL = "https://verifai-backend.onrender.com/";
+}
 
 export default API_BASE_URL;
+
+
+// const backendHost = window.location.hostname;
+// const backendPort = "8000";
+
+// const API_BASE_URL = `http://${backendHost}:${backendPort}/`;
+
+// export default API_BASE_URL;
